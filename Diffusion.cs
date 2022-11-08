e2f2c59c7af7c1d6aaa9ca4b3ba563ccec3d10c3
valou96 <valentinchailloux7@gmail.com>
2022-11-08 13:42:17 +0100

    Ajoutez des fichiers projet.

---
+++ Diffusion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHeritage
+{
+    public abstract class Diffusion
+    {
+        protected String titre;
+        protected int annee;
+
+        public Diffusion(string titre, int annee)
+        {
+            this.titre = titre;
+            this.annee = annee;
+        }
+
+        public String gettitre()
+        {
+            return titre;
+        }
+
+        public int getannee()
+        {
+            return annee;
+        }
+
+        public abstract String getInfos();
+
+    }
+
+
+    public class Serie : Diffusion
+    {
+        private int nbEpisode;
+
+        public Serie(String titre, int annee, int nbEpisode) : base(titre, annee)
+        {
+            this.nbEpisode = nbEpisode;
+        }
+
+        public override string getInfos()
+        {
+            return base.annee + " " + base.titre + "(Nombre d'épisode = " + nbEpisode;
+        }
+
+        public int getnbEpisode()
+        {
+            return nbEpisode;
+        }
+    }
+
+    public class Film : Diffusion
+    {
+        private int recette;
+
+        public Film(String titre, int annee, int recette) : base(titre, annee)
+        {
+            this.recette = recette;
+        }
+
+        public override string getInfos()
+        {
+            return base.annee + " " + base.titre + "(recette = " + recette;
+        }
+
+        public int getRecette()
+        {
+            return recette;
+        }
+    }
+
+   
+
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestHeritage
+{
+    
+    public partial class MainWindow : Window
+    {
+        private List<Diffusion> LesDiffusions = new List<Diffusion>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MajLstDiffusion()
+        {
+            LesDiffusions.Clear();
+            foreach(Diffusion uneDiffusion in LesDiffusions)
+            {
+                LesDiffusions.Add(uneDiffusion);
+                
+            }
+        }
+
+        private void RestrictionAjout()
+        {
+            /*LblRecette.Visibility = RdbFilm.Checked;
+            TxtRecette.Visibility = RdbFilm.Checked;
+            LblNbepisode.Visibility = !RdbFilm.Checked;
+            TxtNbepisode.Visibility = !RdbFilm.Checked;*/
+        }
+        
+
+        private void RdbFilm_checked(object sender, EventArgs e)
+        {
+            RestrictionAjout();
+        }
+
+        private void RdbSerie_checked(object sender, EventArgs e)
+        {
+            RestrictionAjout();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Diffusion uneDiffusion;
+            if ((bool)RdbFilm.IsChecked)
+            {
+                uneDiffusion = new Film(TxtTitre.Text, int.Parse(TxtAnnee.Text), int.Parse(TxtRecette.Text));
+            }
+            else
+            {
+                uneDiffusion = new Serie(TxtTitre.Text, int.Parse(TxtAnnee.Text), int.Parse(TxtNbepisode.Text));
+            }
+            LesDiffusions.Add(uneDiffusion);
+            MajLstDiffusion();
+        }
+    }
+}
